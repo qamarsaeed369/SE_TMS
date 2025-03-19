27c5c81c6e139de0b2f94e81b60cfc2bd0149054
Qamar saeed <qamarsaeed369@gmail.com>
2025-03-19 07:43:15 +0000

    fixed error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
-    "accounts",  # ✅ Corrected reference
+    'accounts',  # ✅ Corrected reference
     "projects",
 
 ]
